d75d4d32df4163ea7997edf44a3d60f42db9160d
luanrf5g <of.luan.rferreira@gmail.com>
2026-03-12 10:12:10 -0300

    fix(back): update origins of cors to app vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:3000'
+      'http://localhost:3000',
+      'https://metalizze.vercel.app'
     ],
     credentials: true,
   })
